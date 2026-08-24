01437ce324ca599be99fad717ef41ee81550a040
Casey Dreier <cdreier@gmail.com>
2026-08-24 10:38:51 -0700

    Add Fathom analytics

--- scripts/bake/templates.mjs
+++ scripts/bake/templates.mjs
@@ -135,6 +135,8 @@ function renderHead({ title, description, canonical, paths }) {
 <link rel="stylesheet" href="${paths.root}shared/css/components.css">
 <link rel="stylesheet" href="${paths.root}shared/css/layout.css">
 <link rel="stylesheet" href="${paths.panels}">
+
+<script src="https://cdn.usefathom.com/script.js" data-site="UDDGKLNZ" defer></script>
 </head>`;
 }
 
